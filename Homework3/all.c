13ecd712f025c2c8d42eba31ce9149f0882fb20c
RoyaM2007 <mahmudovaroya4@gmail.com>
2024-09-26 00:05:05 +0400

    Add files via upload

---
+++ Homework3/all.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int a=15;
+	int b=16;
+	int c=17;
+	int sum=a+b+c;
+	printf("sum%d\n",sum);
+	const Pİ=3.14;
+	printf("const=%f\n",Pİ);
+	printf("a++=%d\n",a++);
+
+}
+
