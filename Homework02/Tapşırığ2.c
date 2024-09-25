c458cfd65e70a1b9e2a1e9ea3aa35ce7769a8cf1
RoyaM2007 <mahmudovaroya4@gmail.com>
2024-09-25 23:50:53 +0400

    Add files via upload

---
+++ Homework02/Tapşırığ2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	 char name[]="Röya";
+	 int i=17;
+	 char world[]="S";
+	 printf("Mənim adım %s dır\n",name);
+	 printf("Mənim %d yaşım var\n",i);
+	 printf("Bu ay %s hərfilə başlayır",world);
+	     return 0;
+	 
+}
+
