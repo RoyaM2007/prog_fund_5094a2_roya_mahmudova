19c2d05590570f0a9be427c7812ea09b4f488d2e
RoyaM2007 <mahmudovaroya4@gmail.com>
2024-09-28 12:42:55 +0400

    Update Tapşırığ2.c

--- Homework02/Tapşırığ2.c
+++ Homework02/Tapşırığ2.c
@@ -3,10 +3,10 @@
 int main() {
 	 char name[]="Röya";
 	 int i=17;
-	 char world[]="S";
-	 printf("Mənim adım %s dır\n",name);
+	 char world='S';
+	 printf("Mənim adım %c dır\n",name);
 	 printf("Mənim %d yaşım var\n",i);
-	 printf("Bu ay %s hərfilə başlayır",world);
+	 printf("Bu ay %c hərfilə başlayır",world);
 	     return 0;
 	 
 }
