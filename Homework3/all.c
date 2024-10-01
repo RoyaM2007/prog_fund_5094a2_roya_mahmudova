39086dc0ffc2f8cc8b6070a2126377a4b0a7004b
RoyaM2007 <mahmudovaroya4@gmail.com>
2024-10-01 10:05:29 +0400

    Update all.c

--- Homework3/all.c
+++ Homework3/all.c
@@ -6,9 +6,9 @@ int main() {
 	int c=17;
 	int sum=a+b+c;
 	printf("sum%d\n",sum);
-	const Pİ=3.14;
+	const float Pİ=3.14;
 	printf("const=%f\n",Pİ);
-	printf("a++=%d\n",a++);
+	printf("%d",++a);
 	return 0;
 
 }
